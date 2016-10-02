18d19474e0f24d643d3ffe2e0abdb4f3c46a251d
Dranithix <dranithix@gmail.com>
2016-10-02 15:02:05 +0800

    Working on image search dialog.

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -13,29 +13,43 @@ Template.profile.helpers({
     }
 })
 
-let lastPress;
+let timeoutQuery = 0, lastQuery;
 
 Template.profile.events({
     'keydown #query'(event) {
-        if (Date.now() - lastPress >= 500) {
-            console.log($(event.currentTarget).val());
-            lastPress = Date.now();
+        if (timeoutQuery != -1) Meteor.clearTimeout(timeoutQuery);
+    },
+    'keyup #query'(event) {
+        let text = $(event.currentTarget).val();
+
+        if (timeoutQuery == -1) {
+            console.log(text);
+            timeoutQuery = Meteor.setTimeout(() => {
+                if (lastQuery && text !== lastQuery) {
+                    // Search for new images.
+
+                    Meteor.call('images.search', text, (err, res) => {
+                        if (!err) {
+                            imageResults.set(res.value);
+
+                            let thumbnailUrls = _.pluck(imageResults.get(), 'thumbnailUrl');
+                        }
+                    });
+
+                    lastQuery = text;
+                }
+                timeoutQuery = -1;
+            }, 500);
         }
     },
 })
 
 Template.profile.onRendered(() => {
-    Meteor.call('images.search', 'natural scenery', (err, res) => {
-        if (!err) {
-            imageResults.set(res.value);
-
-            let thumbnailUrls = _.pluck(imageResults.get(), 'thumbnailUrl');
-
-            $(".grid").isotope({
-                itemSelector: '.grid-item',
-                layoutMode: 'fitRows'
-            });
-        }
-    })
-
+    Template.instance().autorun(() => {
+        imageResults.get();
+        $(".grid").isotope({
+            itemSelector: '.grid-item',
+            layoutMode: 'fitRows'
+        });
+    });
 });
